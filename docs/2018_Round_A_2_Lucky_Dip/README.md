701efc63481957ae3ce271887fd3efa6e553858b
junwoo-lee <junwoo.lee@navercorp.com>
2020-06-14 03:14:19 +0900

    Add 2018-ROUND-A-2 LuckyDip

---
+++ docs/2018_Round_A_2_Lucky_Dip/README.md
@@ -0,0 +1,5 @@
+#### 문제
+- [문제 링크](https://codingcompetitions.withgoogle.com/kickstart/round/0000000000050edf/0000000000050e1d)
+
+#### 설명
+- [설명 링크](https://docs.google.com/document/d/1TC04ySI3yAOxCjsFPYG4gkb4h8-np3s2_3pm9qFNcNc/edit?usp=sharing)
---
+++ docs/2018_Round_A_2_Lucky_Dip/Solution.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+	private static final Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+	private static final PrintWriter w = new PrintWriter(System.out);
+	private static final StringBuilder rs = new StringBuilder();
+
+	private static final String NEW_LINE = "\n";
+
+	public static void main(String[] args) {
+		int T = sc.nextInt();
+		for (int i = 1; i <= T; i++) {
+			int N = sc.nextInt();
+			int K = sc.nextInt();
+			int[] V = new int[N];
+			for (int j = 0; j < N; j++) {
+				V[j] = sc.nextInt();
+			}
+
+			rs.append(String.format("Case #%d: ", i))
+				.append(String.format("%.6f", solve(N, K, V)))
+				.append(NEW_LINE);
+		}
+		w.print(rs.toString());
+		w.close();
+	}
+
+	private static double solve(int n, int k, int[] v) {
+		Arrays.sort(v);
+		double[] partSum = makePartSum(v);
+
+		double curExpectedValue = sumOfElements(v) / n;
+		for (int i = 0; i < k; i++) {
+			int idx = findFirstIdxBiggerThanVal(v, curExpectedValue);
+			double sumOfBigElements = partSum[n] - partSum[idx];
+			curExpectedValue = ((idx * curExpectedValue) / n) + (sumOfBigElements / n);
+		}
+		return curExpectedValue;
+	}
+
+	private static double[] makePartSum(int[] v) {
+		double[] partSum = new double[v.length + 1];
+		partSum[1] = v[0];
+		for (int i = 1; i < v.length; i++) {
+			partSum[i + 1] = partSum[i] + v[i];
+		}
+		return partSum;
+	}
+
+	private static double sumOfElements(int[] elements) {
+		double sumOfElements = 0;
+		for (int element : elements) {
+			sumOfElements += element;
+		}
+		return sumOfElements;
+	}
+
+	private static int findFirstIdxBiggerThanVal(int[] nums, double val) {
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			if (nums[mid] <= val) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return left;
+	}
+}
